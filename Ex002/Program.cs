0b1373e3d59253fa567874dd11548fb02b15ed13
Olga_A <zagaes2@rambler.ru>
2022-10-13 17:22:17 +0300

    created program Ex002

---
+++ Ex002/Program.cs
@@ -0,0 +1,13 @@
+﻿// Подсчитать сумму цифр в числе
+Console.WriteLine("Введите число А= ");
+int a = int.Parse(Console.ReadLine() ?? "0");
+
+int sum = 0;
+int numb = a;
+while (numb>0)
+{
+    sum = sum + (numb%10);
+    numb = numb/10;
+}
+
+Console.WriteLine($"Сумма всех чисел числа {a}= {sum}");
